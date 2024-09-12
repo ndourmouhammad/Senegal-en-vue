b0e521163ceb20480fc7bfb751877275762c3a80
Mouhammad ndour <ndourmouhammad15@gmail.com>
2024-09-12 16:15:46 +0000

    Ajout des permissions

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -155,5 +155,64 @@ public function logout()
     return response()->json(["message" => "Vous avez bien été deconnecté"]);
 }
 
+// Lister les utilisateurs
+ 
+ public function users()
+ {
+     // Charge les utilisateurs avec leurs rôles
+     $users = User::with('roles')->get();
+     
+     // Retourne la réponse JSON personnalisée avec les utilisateurs et leurs rôles
+     return $this->customJsonResponse('Liste des utilisateurs', $users);
+ }
+
+ // Trouver un utilisateur par son ID
+public function user($id)
+{
+    // Trouver l'utilisateur avec ses rôles associés
+    $user = User::with('roles')->find($id);
+    
+    // Vérifiez si l'utilisateur existe
+    if (!$user) {
+        return $this->customJsonResponse('Utilisateur non trouvé', null, 404);
+    }
+    
+    // Retourner la réponse JSON avec l'utilisateur et ses rôles
+    return $this->customJsonResponse('Utilisateur', $user);
+}
+
+ // Supprimer un utilisateur
+ public function destroy($id)
+ {
+     $user = User::find($id);
+     $user->delete();
+     return $this->customJsonResponse('Utilisateur supprimé', $user);
+ }
+
+ // Changer le role d'un utilisateur avec assign role (coach ou entrepreneur)
+ public function changeRole(Request $request, $id)
+ {
+     $user = User::find($id);
+     $user->assignRole($request->role);
+     return $this->customJsonResponse('Role mis a jour', $user);
+ }
+
+ // Activer un utilisateur (en changeant la valeur de son status à 1)
+ public function activate($id)
+ {
+     $user = User::find($id);
+     $user->statut = 1;
+     $user->save();
+     return $this->customJsonResponse('Utilisateur active', $user);
+ }
+
+  // Desactiver un utilisateur (en changeant la valeur de son status à 0)
+  public function deactivate($id)
+  {
+      $user = User::find($id);
+      $user->statut = 0;
+      $user->save();
+      return $this->customJsonResponse('Utilisateur desactive', $user);
+  } 
 }
  
--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,9 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\RoleController;
+use App\Http\Controllers\PermissionController;
+
 
 Route::get('/user', function (Request $request) {
     return $request->user();
@@ -15,3 +18,22 @@ Route::post('/refresh', [AuthController::class, 'refreshToken']);
 Route::get('/me', [AuthController::class, 'userConnecte']);
 Route::post('/update', [AuthController::class, 'update'])->middleware('auth');
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth');
+Route::get('/users', [AuthController::class, 'users']);
+Route::get('/user/{id}', [AuthController::class, 'user']);
+Route::delete('/user/{id}', [AuthController::class, 'destroy']);
+Route::post('/change-role/{id}', [AuthController::class, 'changeRole']);
+Route::post('/activate/{id}', [AuthController::class, 'activate']);
+Route::post('/deactivate/{id}', [AuthController::class, 'deactivate']);
+
+// Permission
+Route::post('/permission', [PermissionController::class, 'ajouterPermission']);
+Route::get('/permissions', [PermissionController::class, 'permissions']);
+Route::post('/permission/{id}', [PermissionController::class, 'modifierPermission']);
+Route::delete('/permission/{id}', [PermissionController::class, 'supprimerPermission']);
+
+// Roles
+Route::get('/roles', [RoleController::class, 'index'])->middleware('permission:lister_roles');
+Route::post('/roles', [RoleController::class, 'store'])->middleware('permission:ajouter_role');
+Route::post('/roles/{id}', [RoleController::class, 'update'])->middleware('permission:modifier_role');
+Route::delete('/roles/{id}', [RoleController::class, 'destroy'])->middleware('permission:supprimer_role');
+Route::post('/roles/{id}/permission', [RoleController::class, 'givePermissions'])->middleware('permission:ajouter_permission');
