c0806d58309cd02889e096c0e7f59ee995cd9307
Mouhammad ndour <ndourmouhammad15@gmail.com>
2024-09-28 14:25:10 +0000

    Mise en place des seeders pour categorie

--- database/seeders/CategorieSeeder.php
+++ database/seeders/CategorieSeeder.php
@@ -11,8 +11,32 @@ class CategorieSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+    public function run()
     {
-        Categorie::factory()->count(5)->create();
+        // Créer des catégories par défaut
+        Categorie::create([
+            'nom' => 'Musique',
+            'description' => 'Concerts, festivals, et autres événements musicaux.'
+        ]);
+
+        Categorie::create([
+            'nom' => 'Art',
+            'description' => 'Expositions, galeries, et événements artistiques.'
+        ]);
+
+        Categorie::create([
+            'nom' => 'Sport',
+            'description' => 'Compétitions sportives, matchs, et événements de fitness.'
+        ]);
+
+        Categorie::create([
+            'nom' => 'Gastronomie',
+            'description' => 'Dégustations, festivals culinaires, et autres événements gastronomiques.'
+        ]);
+
+        Categorie::create([
+            'nom' => 'Culture',
+            'description' => 'Événements culturels, pièces de théâtre, et films.'
+        ]);
     }
 }
